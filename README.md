167daf046b84f1c64dab2b92f05a1414aa10a908
Leandro Ucuamba <104640070+LeandroUcuamba@users.noreply.github.com>
2023-04-18 08:17:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_cookie_storie_app
+ Desenvolvi este " Cookie Store App " utilizando o Flutter.
---
+++ android/app/src/main/kotlin/com/example/flutter_cookie_storie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_cookie_storie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
